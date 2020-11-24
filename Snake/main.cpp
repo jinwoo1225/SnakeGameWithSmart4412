6505bf08a5fb641169f71d729af5a1fb4238ef5e
jinwoo1225 <jinwoo17962@gmail.com>
2020-11-24 12:03:58 +0900

    Initial Commit

---
+++ Snake/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Snake
+//
+//  Created by HongJinwoo on 2020/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
