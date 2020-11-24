72a6280bec60b3326e1bcfbbf1c900195d47afdd
jinwoo1225 <jinwoo17962@gmail.com>
2020-11-24 12:19:18 +0900

    trail을 따르는 함수 추가

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -6,9 +6,54 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef struct coord{
+    int y;
+    int x;
+} coord;
+
+class Snake {
+private:
+    int y,x;
+    int size;
+    vector<coord> trail;
+    
+    
+public:
+    Snake(int y, int x){
+        this->size=1;
+        pushTrail( y, x);
+    }
+    
+    void pushTrail(int y, int x){
+        coord A = {y, x};
+        trail.insert(trail.begin(), A);
+    }
+    
+    vector<coord> getTrail(int size){
+        vector<coord> ret;
+        ret.assign(trail.begin(), trail.begin() + size);
+        return ret;
+    }
+    
+
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    Snake s(4,4);
+    s.pushTrail(5, 6);
+    s.pushTrail(5, 7);
+    s.pushTrail(6, 8);
+    
+    vector<coord> trailT = s.getTrail(3);
+    
+    for (coord i : trailT) {
+        cout << i.y << i.x << endl;
+    }
+    
     return 0;
 }
